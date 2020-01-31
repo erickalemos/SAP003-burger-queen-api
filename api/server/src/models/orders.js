a391d6e1d9f74b55594deef13c0f3be644de6ad6
erickalemos <ericka.florentino@gmail.com>
2020-01-31 17:46:45 -0300

    atualização das funções de product

--- api/server/src/models/orders.js
+++ api/server/src/models/orders.js
@@ -2,6 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const orders = sequelize.define('orders', {
     Clientname: DataTypes.STRING,
+    order:
     table: DataTypes.INTEGER,
     status: DataTypes.STRING,
    
