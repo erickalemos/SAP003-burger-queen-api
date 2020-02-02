7218ec3543ef34ecd4f85a2942c2836fcf720bda
erickalemos <ericka.florentino@gmail.com>
2020-02-02 20:36:30 -0300

    atualização de products

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// app.use('/api/products', productRoutes);
+app.use('/api/products', productRoutes);
 app.use('/api/orders', ordersRoutes);
 //app.use('/api/orderItems', orderItemsRoutes);
 
